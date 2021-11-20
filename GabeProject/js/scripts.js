ca22ba341485733758d2757add15390d3e5d2e3a
G-Aquino <85465477+G-Aquino@users.noreply.github.com>
2021-11-20 14:07:46 -0500

    HW 20 copy

---
+++ GabeProject/js/scripts.js
@@ -0,0 +1,90 @@
+/*!
+* Start Bootstrap - Agency v7.0.10 (https://startbootstrap.com/theme/agency)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+
+var j = jQuery.noConflict();
+j(document).ready(function(){
+  j('#portfolioModal2, #portfolioModal3, #portfolioModal4, #portfolioModal5, #portfolioModal6').hide();
+		j('#radio-1').click(function(){
+		j('#portfolioModal1').show();						  
+		j('#portfolioModal2, #portfolioModal3, #portfolioModal4, #portfolioModal5, #portfolioModal6').hide();					  
+		});
+		
+		j('#radio-2').click(function(){
+		j('#portfolioModal2').show();						  
+		j('#portfolioModal1, #portfolioModal3, #portfolioModal4, #portfolioModal5, #portfolioModal6').hide();					  
+		});
+		
+		j('#radio-3').click(function(){
+        j('#portfolioModal3').show();						  
+        j('#portfolioModal1, #portfolioModal2, #portfolioModal4, #portfolioModal5, #portfolioModal6').hide();					  
+        });
+
+        j('#radio-4').click(function(){
+        j('#portfolioModal4').show();						  
+        j('#portfolioModal1, #portfolioModal2, #portfolioModal3, #portfolioModal5, #portfolioModal6').hide();					  
+        });
+
+        j('#radio-5').click(function(){
+        j('#portfolioModal5').show();						  
+        j('#portfolioModal1, #portfolioModal2, #portfolioModal3, #portfolioModal4, #portfolioModal6').hide();					  
+        });
+
+        j('#radio-6').click(function(){
+        j('#portfolioModal6').show();						  
+        j('#portfolioModal1, #portfolioModal2, #portfolioModal3, #portfolioModal4, #portfolioModal5').hide();					  
+        });
+									 			  
+});
